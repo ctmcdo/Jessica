cbc11a7c8fcf91f9135e2c47ca1998a42937a260
ctmcdo <24464477+ctmcdo@users.noreply.github.com>
2022-12-06 22:30:27 +0000

    Update filter_pawn.h

--- filter_pawn/filter_pawn.h
+++ filter_pawn/filter_pawn.h
@@ -6,8 +6,8 @@
 extern "C" {
 #endif
 bool FilterPawn(uint64_t pawns[NUM_SIDES], uint64_t enpassant,
-                int num_captuable_chessmen[NUM_SIDES],
-                int min_promotions[NUM_SIDES]);
+                int num_captured_base_pieces[NUM_SIDES],
+                int min_promotions_to_acc_for[NUM_SIDES]);
 #ifdef _cplusplus
 }
 #endif
